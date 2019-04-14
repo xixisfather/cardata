fc71a8b020db040251cb87925f38498f3792957b
chenchunrong <chenchunrong@yingle.com>
2019-04-14 16:14:04 +0800

    no message

---
+++ cardata/src/main/java/com/car/analyse/CardataApplication.java
@@ -0,0 +1,13 @@
+package com.car.analyse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardataApplication.class, args);
+	}
+
+}
